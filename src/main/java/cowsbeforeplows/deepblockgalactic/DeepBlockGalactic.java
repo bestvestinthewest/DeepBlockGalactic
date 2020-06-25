d1c0d31960e10b64704a052c49158270ce9d1f6b
cowsbeforeplows <bestvestinthewest@gmail.com>
2020-06-25 02:15:27 -0500

    Basic Minerals for Hoxxes IV
    Added Nitra, Morkite, and Dwarven Gold with temporary textures

--- src/main/java/cowsbeforeplows/deepblockgalactic/DeepBlockGalactic.java
+++ src/main/java/cowsbeforeplows/deepblockgalactic/DeepBlockGalactic.java
@@ -1,22 +1,42 @@
 package cowsbeforeplows.deepblockgalactic;
 
+import cowsbeforeplows.deepblockgalactic.init.ItemInit;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod("deepblockgalactic")
 public class DeepBlockGalactic {
 
+	public static final String MOD_ID = "deepblockgalactic";
+	public static DeepBlockGalactic instance;
+	
+	
 	
 	public DeepBlockGalactic() {
+		
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         
+        ItemInit.ITEMS.register(modEventBus);
+        
+        instance = this;
         MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@SubscribeEvent
+	public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
+		final IForgeRegistry<Item> registy = event.getRegistry();
+		
 	}
 	
 	private void setup(final FMLCommonSetupEvent event) {
--- src/main/java/cowsbeforeplows/deepblockgalactic/init/ItemInit.java
+++ src/main/java/cowsbeforeplows/deepblockgalactic/init/ItemInit.java
@@ -1,5 +1,26 @@
 package cowsbeforeplows.deepblockgalactic.init;
 
+import cowsbeforeplows.deepblockgalactic.DeepBlockGalactic;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ItemInit {
+	
+	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, DeepBlockGalactic.MOD_ID);
+
+	
+	
+	// Minerals
+	public static final RegistryObject<Item> NITRA = ITEMS.register("nitra", () -> new Item(new Item.Properties().maxStackSize(80).group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> MORKITE = ITEMS.register("morkite", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> GOLD = ITEMS.register("gold", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+
+	
+	
 
 }
